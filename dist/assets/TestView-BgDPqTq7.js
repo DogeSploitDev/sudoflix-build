8dadbc40ccdeb28fd218ad09572ab1fdf0b81adf
Alex P <alppiguet@outlook.com>
2025-03-04 16:19:54 +0000

    Add dist

---
+++ dist/assets/TestView-BgDPqTq7.js
@@ -0,0 +1,2 @@
+import{r as e,j as s}from"./vendor-CRD5yWxp.js";import{B as o}from"./index-bxoUcoqi.js";function i(){const[r,t]=e.useState(!1);if(r)throw new Error("I crashed");return s.jsx(o,{onClick:()=>t(!0),children:"Crash me!"})}export{i as default};
+//# sourceMappingURL=TestView-BgDPqTq7.js.map
